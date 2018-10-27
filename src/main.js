06171345d193ff898a427f95cc95383a721cce7b
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2018-10-28 00:15:25 +0900

    refactor: :+1: 表記揺れ統一

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import DefaultLayout from "~/layouts/Default.vue";
+import DefaultLayout from "@/layouts/Default.vue";
 
-import "~/styles/core/reset.scss";
-import "~/styles/core/base.scss";
+import "@/styles/core/reset.scss";
+import "@/styles/core/base.scss";
 
 export default function(Vue) {
   Vue.component("Layout", DefaultLayout);
