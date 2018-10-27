9b4d0be40c19b9e81f6f51af30d408446d406344
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2018-10-27 23:03:21 +0900

    refactor: :+1: googlefonts導入

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -17,6 +17,26 @@
 	</div>
 </template>
 
+<script>
+export default {
+  metaInfo: {
+    htmlAttrs: {
+      lang: "ja"
+    },
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" }
+    ],
+    link: [
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css?family=Noto+Sans+JP"
+      }
+    ]
+  }
+};
+</script>
+
 <style lang="scss">
 body {
   padding: 0;
