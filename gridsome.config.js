452321d10fdbf7115f973b43967117dc8c5df69d
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2018-10-29 23:52:02 +0900

    refactor: :tada: dotenvで環境変数作成

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,5 +1,24 @@
 const path = require("path");
+
+// use dotenv
+require("dotenv").config();
+
 module.exports = {
+  plugins: [
+    {
+      use: "@gridsome/source-wordpress",
+      options: {
+        baseUrl: process.env.VUE_APP_BASE_URL,
+        typeName: "WordPress",
+        perPage: 100,
+        concurrent: 10,
+        routes: {
+          post: "/:year/:month/:day/:slug",
+          post_tag: "/tag/:slug"
+        }
+      }
+    }
+  ],
   siteName: "tyankatsu",
   chainWebpack: config => {
     const oneOfsMap = config.module.rule("scss").oneOfs.store;
